4d8a734d0f9b392e0479cc592415547f816b69ac
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-05-17 01:10:13 +0530

    Changes in next config for webpack svgs

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
     rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
+  },
+  webpack(config) {
+    config.module.rules.push({ test: /\.svg$/, use: ["@svgr/webpack"] });
+    return config;
   },
 };
 
